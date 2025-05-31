ceef4d8dbea22fd8165095483f49fe152e82e253
Sebastian Quintero <josequinterosebastian@gmail.com>
2025-05-31 00:12:34 -0400

    Modificando el Main para escribir

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         
-        
-        System.out.println("Hola, mundo!");
+        System.out.println("Luciano es gay y le gusta el chocolate");
+        System.out.println("Luciano es el amor de mi vida");
     }
 }
