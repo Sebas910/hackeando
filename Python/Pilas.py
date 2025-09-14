7d8cbb00e9fb8fc2b912b7e2f690f8281c89af3b
Sbxz <josequinterosebastian@gmail.com>
2025-09-14 10:44:10 -0300

    cambios en el Main.java y adicion de la clase stack en Python

---
+++ Python/Pilas.py
@@ -0,0 +1,31 @@
+class Node: 
+    def __init__(self, value):
+        
+        self.value = value
+        self.next = None   
+    
+class Stack:
+   
+   def __init__(self):
+        
+        self.top = None
+
+   def push(self,value):
+        
+        new_node = Node(value)
+        
+        new_node.next = self.top
+        
+        self.top = new_node
+    
+   def pop(self):
+        
+     if self.top is None:
+         
+        raise IndexError("esta vacio el stack")
+    
+     value = self.top.value
+    
+     self.top = self.top.next
+     
+     return value         
